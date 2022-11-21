053385d641dbca57f3c8c629963d864302dedc26
Mineinjava <65673396+Mineinjava@users.noreply.github.com>
2022-11-21 10:51:52 -0800

    move nested function

--- blobcheck.py
+++ blobcheck.py
@@ -3,32 +3,33 @@ import border
 import copy
 
 
+def solve(matrix):
+    def dfs(i, j):
+        if i < 0 or j < 0 or i >= R or j >= C:
+            return True
+        if matrix[i][j] == 0:
+            return True
+        matrix[i][j] = 0
+        a = dfs(i + 1, j)
+        b = dfs(i - 1, j)
+        c = dfs(i, j + 1)
+        d = dfs(i, j - 1)
+        return a and b and c and d
+
+    R, C = len(matrix), len(matrix[0])
+    ans = 0
+    for i in range(R):
+        for j in range(C):
+            if matrix[i][j] == 1:
+                if dfs(i, j):
+                    ans += 1
+    return ans
+
+
 def blobs(pic):
     borderpic = border.bordercheck(pic)
     fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(10, 6))
     islandpic = copy.deepcopy(pic)
-
-    def solve(matrix):
-        def dfs(i, j):
-            if i < 0 or j < 0 or i >= R or j >= C:
-                return True
-            if matrix[i][j] == 0:
-                return True
-            matrix[i][j] = 0
-            a = dfs(i + 1, j)
-            b = dfs(i - 1, j)
-            c = dfs(i, j + 1)
-            d = dfs(i, j - 1)
-            return a and b and c and d
-
-        R, C = len(matrix), len(matrix[0])
-        ans = 0
-        for i in range(R):
-            for j in range(C):
-                if matrix[i][j] == 1:
-                    if dfs(i, j):
-                        ans += 1
-        return ans
 
     islands = solve(islandpic)
 
@@ -39,5 +40,8 @@ def blobs(pic):
     print("Islands: " + str(islands))
     plt.show()
 
-blobs(50, 5)
 
+if __name__ == "__main__":
+    from terraingen import terrain
+
+    blobs(terrain(5, 1, 50, 50))
