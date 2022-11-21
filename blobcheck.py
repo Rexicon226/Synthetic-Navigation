d3b45117f88a83f1c79de857adb1b98f1e7e5411
Rexicon226 <daviru007@icloud.com>
2022-11-20 21:49:38 -0800

    plz fixer

--- blobcheck.py
+++ blobcheck.py
@@ -1,6 +1,7 @@
 import pathcheck
 import terraingen
 import matplotlib.pyplot as plt
+import border
 
 pic = pathcheck.path(15, 3)
 plt.imshow(pic, cmap='gray')
---
+++ border.py
@@ -0,0 +1,43 @@
+import copy
+
+import matplotlib.pyplot as plt
+
+import pathcheck
+
+pic = pathcheck.path(40, 4)
+plt.imshow(pic, cmap='gray')
+# plt.imshow(borderpic, cmap='Blues')
+plt.show()
+borderpic = copy.deepcopy(pic)  # create a deep copy so that stuff doesn't get messed up
+for x in range(len(pic)):
+    for y in range(len(pic)):
+        if pic[x][y] == 1:
+            mx = 0
+            mn = 0
+            if y < len(pic) - 1:
+                mx += 1
+                mn += pic[x][y + 1]
+            if y > 0:
+                mx += 1
+                mn += pic[x][y - 1]
+            if x < len(pic) - 1:
+                mx += 1
+                mn += pic[x + 1][y]
+            if x > 0:
+                mx += 1
+                mn += pic[x - 1][y]
+
+            if mx != mn:
+                borderpic[x][y] = 1
+            else:
+                borderpic[x][y] = 0
+
+        else:
+            pic[x][y] = 0
+
+            # borderpic[x][y] = mx-1
+print(pic)
+print(borderpic)
+# plt.imshow(pic, cmap='gray')
+plt.imshow(borderpic, cmap='Blues')
+plt.show()
