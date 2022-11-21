89f30bc01daaae49cb37041ad6ad829002c337fe
Mineinjava <65673396+Mineinjava@users.noreply.github.com>
2022-11-21 11:19:38 -0800

    add docstring

--- terraingen.py
+++ terraingen.py
@@ -2,7 +2,25 @@ import numpy as np
 from perlin_noise import PerlinNoise
 
 
-def terrain(octaves, seed, x, y):
+def terrain(octaves: int, seed, x, y):
+    """generates a random "terrain" map
+
+    Parameters
+    ----------
+    octaves : int
+        octaves for perlin noise
+    seed : int
+        random seed
+    x : int
+        x size of the array
+    y : int
+        y size of the array
+
+    Returns
+    -------
+    pic : list[list[0,1]]
+        thresholded list of smooth noise
+    """
     noise = PerlinNoise(octaves=octaves, seed=seed)
     xpix, ypix = x, y
     pic = [[noise([i / xpix, j / ypix]) for j in range(xpix)] for i in range(ypix)]
