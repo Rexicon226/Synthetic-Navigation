9cc212250fbd175735b3bd0e6c8c859f9a048cce
Mineinjava <fbisurveillancevan525178@gmail.com>
2022-11-20 21:20:44 -0800

    got border recognition to work

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-<<<<<<< HEAD
 import matplotlib.pyplot as plt
 from perlin_noise import PerlinNoise
+import copy
 
 # define the noise
 noise = PerlinNoise(octaves=10, seed=1)
 # define the size
 xpix, ypix = 100, 100
-threshold_amount = 0.5 # variable to round to (above this is black, below is white)
+threshold_amount = 0.5  # variable to round to (above this is black, below is white)
 
-def threshold(value: float, amt:float) -> int:
+
+def threshold(value: float, amt: float) -> int:
     """converts the value to a 0 if it is less than amt, or 1 if it is greater than or equal to amt"""
     return int(value >= amt)
+
 
 def thresholdedNoise(x: float, y: float, noise_func: PerlinNoise) -> int:
     """converts the perlinNoise output to a threshold (0 or 1)"""
@@ -21,13 +23,38 @@ def thresholdedNoise(x: float, y: float, noise_func: PerlinNoise) -> int:
 
 
 pic = [[thresholdedNoise(i / xpix, j / ypix, noise) for j in range(xpix)] for i in range(ypix)]
+borderpic = copy.deepcopy(pic)
+for x in range(len(pic)):
+    for y in range(len(pic)):
+        if pic[x][y] == 1:
+            mx = 0
+            mn = 0
+            if y < len(pic) - 1:
+                mx += 1
+                mn += pic[x][y + 1]
+            if y > 0:
+                mx += 1
+                mn += pic[x][y - 1]
+            if x < len(pic) - 1:
+                mx += 1
+                mn += pic[x + 1][y]
+            if x > 0:
+                mx += 1
+                mn += pic[x - 1][y]
 
+            if mx != mn:
+                borderpic[x][y] = 1
+            else:
+                borderpic[x][y] = 0
+
+
+
+        else:
+            pic[x][y] = 0
+
+            # borderpic[x][y] = mx-1
+print(pic)
+print(borderpic)
 plt.imshow(pic, cmap='gray')
+plt.imshow(borderpic, cmap='gray')
 plt.show()
-=======
-def main(name):
-    print(name)
-
-
-main("test")
->>>>>>> e77fb6c (first test)
