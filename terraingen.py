7e7cddb341f91a40ecd9e5b583956e469a365449
Mineinjava <65673396+Mineinjava@users.noreply.github.com>
2022-11-21 10:33:30 -0800

    rename "size" to "octaves"

--- terraingen.py
+++ terraingen.py
@@ -2,12 +2,11 @@ import numpy as np
 from perlin_noise import PerlinNoise
 
 
-def terrain(size, randseed, x, y):
-    noise = PerlinNoise(octaves=size, seed=randseed)
+def terrain(octaves, randseed, x, y):
+    noise = PerlinNoise(octaves=octaves, seed=randseed)
     xpix, ypix = x, y
     pic = [[noise([i / xpix, j / ypix]) for j in range(xpix)] for i in range(ypix)]
     for i in range(xpix):
         for j in range(ypix):
             pic[i][j] = np.floor(pic[i][j])
     return pic
-
