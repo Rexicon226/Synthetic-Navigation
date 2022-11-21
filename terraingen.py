4003bb19458a3fc5d5d8e21ebfa481994b4504f7
Mineinjava <65673396+Mineinjava@users.noreply.github.com>
2022-11-21 10:34:00 -0800

    rename other things

--- terraingen.py
+++ terraingen.py
@@ -2,8 +2,8 @@ import numpy as np
 from perlin_noise import PerlinNoise
 
 
-def terrain(octaves, randseed, x, y):
-    noise = PerlinNoise(octaves=octaves, seed=randseed)
+def terrain(octaves, seed, x, y):
+    noise = PerlinNoise(octaves=octaves, seed=seed)
     xpix, ypix = x, y
     pic = [[noise([i / xpix, j / ypix]) for j in range(xpix)] for i in range(ypix)]
     for i in range(xpix):
