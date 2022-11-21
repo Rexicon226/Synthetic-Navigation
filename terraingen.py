15846ba2f0e643c08a4ed6b4773c4bbf2f4affb3
Mineinjava <65673396+Mineinjava@users.noreply.github.com>
2022-11-21 11:20:03 -0800

    avoid iterating twice

--- terraingen.py
+++ terraingen.py
@@ -23,8 +23,6 @@ def terrain(octaves: int, seed, x, y):
     """
     noise = PerlinNoise(octaves=octaves, seed=seed)
     xpix, ypix = x, y
-    pic = [[noise([i / xpix, j / ypix]) for j in range(xpix)] for i in range(ypix)]
-    for i in range(xpix):
-        for j in range(ypix):
-            pic[i][j] = np.floor(pic[i][j])
+    pic = [[np.floor(noise([i / xpix, j / ypix])) for j in range(xpix)] for i in range(ypix)]
+
     return pic
