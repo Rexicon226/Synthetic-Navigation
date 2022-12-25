9c316125cd489fe02d5ec13f02094a6345d9d17f
David <daviru007@icloud.com>
2022-12-24 22:01:57 -0800

    bruv this quite hard' ok cnn is the way cnn > gan im figuring out the bugs from this bs code doesnt work

--- ML/array2img.py
+++ ML/array2img.py
@@ -4,19 +4,39 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 from tqdm import tqdm
+import cv2
+import PIL
+from skimage import data, io, util
 
 
 def array2image(x, y, octaves, weight, seed: int = 0, iD: int = 0):
     array = generator.generateClean(x, y, octaves, seed)
-    plt.imsave('clean/' + str(iD) + '_image.png', array, cmap='binary')
-    array = generator.generateNoise(x, y, octaves, weight,seed)
-    plt.imsave('noise/' + str(iD) + '_image.png', array, cmap='binary')
+    bool_array = np.array(array, dtype=bool)
+    img = PIL.Image.fromarray(bool_array)
+    img.save('./train_images/clean/' + str(iD) + '_image.png', bits=1, optimize=True)
+    array = generator.generateNoise(x, y, octaves, weight, seed)
+    bool_array = np.array(array, dtype=bool)
+    img = PIL.Image.fromarray(bool_array)
+    img.save('./train_images/noise/' + str(iD) + '_image.png', bits=1, optimize=True)
 
 
-image_count = 100
+image_count = 1000
 
-for i in tqdm(range(image_count)):
-    seed = random.randint(1, 10000000000)
-    array2image(50, 50, 4, 10, seed, i)
 
+def greyscale():
+    for i in tqdm(range(image_count)):
+        image = cv2.imread('./images/noise/inputs/' + str(i) + '_image.png')
+
+        gray_image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+
+        plt.imsave('images/noise/inputs/' + str(i) + '_image.png', gray_image, cmap='gray', bits=1)
+
+
+def generate():
+    for i in tqdm(range(image_count)):
+        seed = random.randint(1, 10000000000)
+        array2image(50, 50, 4, 10, seed, i)
+
+
+generate()
 print("done")
--- ML/main.py
+++ ML/main.py
@@ -1,96 +1,106 @@
-import os
-
 import torch
+import torch.nn as nn
 import torch.optim as optim
-from PIL import Image
-from torch import nn
+import torchvision
+import torch.utils.data
+from torch.utils.data import Subset, TensorDataset, DataLoader
+from torchvision.datasets import ImageFolder
 from torch.utils.data import Dataset
-from torchvision import transforms
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-
-# Define the CNN model
-class NoiseFilter(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
-        self.pool = nn.MaxPool2d(2)
-        self.fc = nn.Linear(64 * 7 * 7, 1)
-
-    def forward(self, for_x):
-        for_x = self.pool(torch.relu(self.conv1(for_x)))
-        for_x = self.pool(torch.relu(self.conv2(for_x)))
-        for_x = for_x.view(-1, 64 * 7 * 7)
-        for_x = torch.sigmoid(self.fc(for_x))
-        return for_x
-
-
-class ImageDataset(Dataset):
-    def __init__(self, root_dir, init_transform=None):
-        self.root_dir = root_dir
-        self.transform = init_transform
-        self.images = []
-        self.labels = []
-
-        # Load the image and label data
-        for filename in os.listdir(root_dir):
-            # Extract the label from the filename (e.g., "1_image.jpg" -> 1)
-            label = int(filename.split("_")[0])
-            self.labels.append(label)
-            self.images.append(os.path.join(root_dir, filename))
-
-    def __len__(self):
-        return len(self.images)
-
-    def __getitem__(self, idx):
-        get_image = Image.open(self.images[idx])
-        label = self.labels[idx]
-        if self.transform:
-            get_image = self.transform(get_image)
-        return get_image, label
-
-
-transform = transforms.Compose([
-    transforms.Resize((32, 32)),
-    transforms.ToTensor(),
-    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+# 1. Prepare the data
+# Load the binary images and apply any preprocessing steps as needed
+transform = torchvision.transforms.Compose([
+    torchvision.transforms.Resize((50, 50)),
+    torchvision.transforms.ToTensor(),
+    torchvision.transforms.Grayscale()
 ])
 
-dataset = ImageDataset("clean/", init_transform=transform)
-dataloader = torch.utils.data.DataLoader(dataset, batch_size=32, shuffle=True)
 
-# Initialize the model and the optimizer
-model = NoiseFilter()
+# 2. Define the CNN model
+class NoiseFilterCNN(nn.Module):
+    def __init__(self):
+        super(NoiseFilterCNN, self).__init__()
+
+        # Define the convolutional layers
+        self.conv1 = nn.Conv2d(1, 16, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+
+        # Define the max pooling layer
+        self.pool = nn.MaxPool2d(2, 2)
+
+        # Define the fully connected layers
+        self.fc1 = nn.Linear(64 * 48 * 48, 128)
+        print(self.fc1)
+        self.fc2 = nn.Linear(128, 1)
+
+        # Define the activation function
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        # Apply the convolutional layers and max pooling
+        x = self.pool(self.relu(self.conv1(x)))
+        x = self.pool(self.relu(self.conv2(x)))
+        x = self.pool(self.relu(self.conv3(x)))
+
+        print(x.shape)
+
+        # Flatten the output of the convolutional layers
+        x = x.view(-1, 64 * 48 * 48)
+
+        print(x.shape, x)
+        # Apply the fully connected layers
+        x = self.relu(self.fc1(x))
+        x = self.fc2(x)
+
+        return x
+
+
+model = NoiseFilterCNN()
+model = model.to(device)
+
+train_dataset = ImageFolder('./train_images/', transform=transform)
+val_dataset = ImageFolder('./val_images/', transform=transform)
+train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=64, shuffle=False)
+
+num_epochs = 100
+
+# Define the loss function and optimizer
+criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters())
-
-num_epochs = 1000
 
 # Train the model
 for epoch in range(num_epochs):
-    model.train()
-    for x, y in dataloader:
+    # Iterate over the training data
+    for inputs, labels in train_loader:
+        # Move the data to the device
+        inputs = inputs.to(device)
+        labels = labels.to(device)
+
+        # Zero the gradients
         optimizer.zero_grad()
-        y_pred = model(x)
-        loss = nn.BCEWithLogitsLoss()(y_pred, y)
+
+        # Forward pass
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+
+        # Backward pass and optimization
         loss.backward()
         optimizer.step()
 
-# Evaluate the model on the test set
-model.eval()
-with torch.no_grad():
-    for x, y in dataloader:
-        y_pred = model(x)
-        acc = (y_pred > 0.5).eq(y).float().mean()
-
-# Use the model to filter noise from a new image
-image = ...  # your binary image with noise
-image = image.unsqueeze(0).unsqueeze(0)  # add dummy batch and channel dimensions
-image = image.to(device)  # move image to the device (e.g., GPU) if available
-model.eval()
-with torch.no_grad():
-    y_pred = model(image).squeeze().cpu().numpy()
-image_clean = (y_pred > 0.5).astype(int)  # filter out pixels predicted to be noise
-
-print(image_clean)
+    # Evaluate the model on the validation data
+    with torch.no_grad():
+        correct = 0
+        total = 0
+        for inputs, labels in val_loader:
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+            outputs = model(inputs)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+        val_acc = correct / total
+        print(f'Epoch {epoch + 1}: Validation accuracy = {val_acc:.2f}')
