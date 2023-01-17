15adb2908bf86fc688602d3f34acc202b7811c9f
David <daviru007@icloud.com>
2023-01-16 19:18:10 -0800

    vroom more

--- Pathfinding/astar.py
+++ Pathfinding/astar.py
@@ -5,8 +5,6 @@ import heapq
 
 import random
 
-from tqdm import tqdm
-
 from Terrain.timers import FunctionTimer
 from CompositeEnvironment import Visualizer, Environment
 from Terrain import pathcheck, generator
@@ -19,60 +17,63 @@ class AStarPathfinder:
         self.end = (255, 255)
         self.visited = set()
         self.path = []
-        self.fig, self.ax = plt.subplots()
+        self.fig, self.ax = plt.subplots(1, 2)
 
     @staticmethod
     def heuristic(a, b):
         # Manhattan distance heuristic
         return abs(a[0] - b[0]) + abs(a[1] - b[1])
 
-
     def a_star(self):
         queue = []
         heapq.heappush(queue, (self.heuristic(self.start, self.end), self.start))
         # Distance threshold
-        max_distance = 256*256
+        max_distance = 256 * 256
         visited = set()
         visited.add(self.start)
-        with tqdm(total=256*256) as pbar:
-            while queue:
-                current_distance, current = heapq.heappop(queue)
 
-                if current_distance > max_distance:
-                    break
+        while queue:
+            current_distance, current = heapq.heappop(queue)
 
-                pbar.update(1)
+            if current_distance > max_distance:
+                break
 
-                if current == self.end:
-                    break
+            if current == self.end:
+                break
 
-                for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
-                    x, y = current
-                    neighbor = (x + dx, y + dy)
+            for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                x, y = current
+                neighbor = (x + dx, y + dy)
 
-                    if (0 <= neighbor[0] < 256) and (0 <= neighbor[1] < 256):
-                        if self.terrain[neighbor[0], neighbor[1]] == 0 and neighbor not in visited:
-                            distance = self.heuristic(neighbor, self.end)
-                            heapq.heappush(queue, (distance, neighbor))
-                            self.path.append(neighbor)
-                            visited.add(neighbor)
+                if (0 <= neighbor[0] < 256) and (0 <= neighbor[1] < 256):
+                    if self.terrain[neighbor[0], neighbor[1]] == 0 and neighbor not in visited:
+                        distance = self.heuristic(neighbor, self.end)
+                        heapq.heappush(queue, (distance, neighbor))
+                        self.path.append(neighbor)
+                        visited.add(neighbor)
 
     def animate(self):
-        im = self.ax.imshow(self.terrain, animated=True)
+        im = self.ax[0].imshow(self.terrain, animated=True)
+        self.ax[1].imshow(masked, animated=False)
+        self.ax[0].set_title("A* Pathfinding")
+        self.ax[1].set_title("Input")
+
+        self.fig.suptitle("Image Size: 256 x 256\nNoise Level: {}%\nAccuracy: {:.2f}%".format(noise_level, loss),
+                          fontsize=16, y=0.9)
 
         def updatefig(*args):
             if self.path:
                 x, y = self.path.pop(0)
                 im.set_array(self.terrain)
-                self.ax.scatter(y, x, c='r', marker='x')
+                self.ax[0].scatter(y, x, c='r', marker='x', s=1)
 
             else:
                 im.set_array(self.terrain)
-                self.ax.scatter(self.end[1], self.end[0], c='g', marker='x')
+                self.ax[0].scatter(self.end[1], self.end[0], c='g', marker='x', s=1)
                 return [im]
 
         ani = animation.FuncAnimation(self.fig, updatefig, interval=1)
-        ani.save('astar.mp4', writer='imagemagick', fps=60)
+        ani.save('astar.mp4', writer='ffmpeg', fps=480, dpi=1000)
         plt.show()
 
 
