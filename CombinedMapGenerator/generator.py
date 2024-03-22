6a0cbd9b148ad953e621eab77c957918a50eb5b5
David Rubin <daviru007@icloud.com>
2024-03-21 19:43:15 -0700

    remove the `CombinedMapGenerator` as there isn't anything there.
    REDO this later!

--- CombinedMapGenerator/generator.py
+++
@@ -1,7 +0,0 @@
-"""Generator used to make map pairs"""
-
-class Generator:
-    def __init__(self, width, height, octaves=1):
-        self.width = width
-        self.height = height
-        self.octaves = octaves
